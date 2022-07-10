dfc342ba9df687bff87feaebfc4cf6281f49acbc
MinhVu88 <minhvu72826@gmail.com>
2022-07-11 02:36:50 +0700

    2nd commit

---
+++ JUnit5/src/main/java/Grader.java
@@ -0,0 +1,17 @@
+public class Grader {
+	public char getLetterGrade(int numericGrade) {
+		if(numericGrade < 0) {
+			throw new IllegalArgumentException("numeric grade must be positive");
+		} else if(numericGrade < 60) {
+			return 'F';
+		} else if(numericGrade < 70) {
+			return 'D';
+		} else if(numericGrade < 80) {
+			return 'C';
+		} else if(numericGrade < 90) {
+			return 'B';
+		} else {
+			return 'A';
+		}
+	}
+}
